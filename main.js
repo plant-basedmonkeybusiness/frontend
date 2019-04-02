ed61be1a78f9523c9501a0176086db8637f1e4b5
Sigrún Líf Erlingsdóttir <borgar@Sigrun-Lifs-Air.tskoli.is>
2019-04-02 09:12:33 +0000

    added scientific name

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 const db = firebase.firestore();
 const mainDiv = document.querySelector('#main'); 
 
-db.collection("images").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        const data = doc.data();
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        mainDiv.innerHTML += `
-        <h1>${data.name}</h1>
-        <span>${data.year}</span>
-        <span>${data.rate}</span>
-        <img src="${data.url}">
+fetch("http://localhost:3000/plants")
+.then(r => r.json())
+.then(data=>{
+
+    data.forEach( data =>{
+        mainDiv.innerHTML +=
+        
         `
-    });
-});
+        <div class="card">
+            <span>${data.scientific_name}</span>
+        </div>
+        `
+    })
+})
+
+
+// db.collection("images").get().then(function(querySnapshot) {
+//     querySnapshot.forEach(function(doc) {
+//         const data = doc.data();
+//         // doc.data() is never undefined for query doc snapshots
+//         console.log(doc.id, " => ", doc.data());
+//         mainDiv.innerHTML += `
+//         <h1>${data.name}</h1>
+//         <span>${data.year}</span>
+//         <span>${data.rate}</span>
+//         <img src="${data.url}">
+//         `
+//     });
+// });
