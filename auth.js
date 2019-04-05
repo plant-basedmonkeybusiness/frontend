f02e4d1b368621485dbdfc4794e1c7463df541de
0 <aesa64@gmail.com>
2019-04-05 14:34:43 +0000

    lokaniðurstaða 14:30 friday

--- auth.js
+++ auth.js
@@ -1,8 +1,12 @@
  // Modal Sign up
+ // Búa til breytur til að sækja modal og takka frá HTML  
 const modal = document.querySelector('#modal-signup');
 const modalOpen = document.querySelector('#sign-up-btn');
 const modalClose = document.querySelector('#close-btn-signup');
 
+// Bæta við 'click event' á sign-up takkann til að virkja modalinn/ sýna hann með klasa sem heitir active. 
+// og sá klasi er með display = block í CSS. 
+// Hann lokast með 'click-event' á modalClose sem er "X" uppi í hægra horni 
 modalOpen.addEventListener('click', ()=> {
   modal.classList.add('active');
 });
@@ -11,6 +15,7 @@ modalClose.addEventListener('click', ()=> {
 });
 
  // Modal Log in
+ // alveg það sama og signUp nema fyrir LogIn 
  const logInModal = document.querySelector('#modal-login');
  const openLogInModal = document.querySelector('#login-btn');
  const closeLogInModal = document.querySelector('#close-btn-login');
@@ -23,24 +28,29 @@ modalClose.addEventListener('click', ()=> {
  });
  
 // signup
+// Þetta er formið í Modal glugganum, input svæði til að skrá sig. 
+// submit er með default virkni sem refresh-ar gluggann,  við tökum hann af með e.preventDefault
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
   // get user info
+  // Til að ná í value-input frá notandanum
+  // signUpForm er object, og property geta ekki verið með bandstriki svo þessu uppsetning er þörf.
+  // þetta er ekki array heldur bara vegna bandstriks í signup-email. 
+  
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
   // sign up the user
+  // firebase býr til notanda hér 
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     console.log(cred);
-    // close the signup modal & reset form
-    // const modal = document.querySelector('#modal-signup');
-    // M.Modal.getInstance(modal).close();
-    // signupForm.reset();
+   
   });
 });
 // logout
+// það sama og signIn og signUp, 
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
@@ -50,15 +60,18 @@ logout.addEventListener('click', (e) => {
   })
 });
 // login
+// sama og í línu 33 
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
   // get user info
+  // sama og í línu 42
   const email = loginForm['login-email'].value;
   const password = loginForm['login-password'].value;
 
   // log the user in
+  // sama og línu 47
   auth.signInWithEmailAndPassword(email, password)
   .then((cred) => {
     console.log(cred.user);
@@ -70,3 +83,4 @@ loginForm.addEventListener('submit', (e) => {
 
   });
 });
+// Sigrún + Sunna 
--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const db = firebase.firestore();
 const auth = firebase.auth();
 const mainDiv = document.querySelector('#main');
 const myArray = [];
+// hér er tómt array því við push-um data upplýsingunum inn í. 
 const priceFilter = document.querySelector("#price-filter");
 const alphabet = document.querySelector("#alphabet-sort");
+
+// Þetta er function sem skilar korti og tekur inn data sem parameter 
+//  
 const addCard = (data) => {
     mainDiv.innerHTML += `
     <div class="card">
@@ -18,31 +22,52 @@ const addCard = (data) => {
         <p class="description">${data.info}</p>
     </div>
     `
+    
 }
+// hér erum við að setja cardið inn í firebase 
+//querySnapshot kemur úr firebase, og er tengt því collection-i. 
 db.collection("plants").get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             const data = doc.data();
+            // doc.data , þá fæðu aðgang að öllum property-um 
+            //úr hverju staki í array
             myArray.push(data);
             addCard(data);
         })
     });
+
+    // þetta er event-listener á takkann til að taka út verð sem eru yfir $25
+    // til þess er notað filter arrowfunction sem return-ar, þess vegna er ekki curly braces 
 priceFilter.addEventListener("click", (e) => {
     const price = myArray.filter(data => data.price < 25)
     mainDiv.innerHTML = "";
     price.forEach(plant => {
+        // í klassískri forlúppu væri plant =  price[i] 
+        // forEach er forlykkja sem fer í gegnum öll price tög inni í array og skilar þeim stökum sem 
+        // falla undir skilyrðið "undir 25"
+        // svo kallað á cardið hér að neðan. 
         addCard(plant);
     })
 });
+    // hér er sett click-event á sort takkann, til að breyta úr stafrófsröð comName yfir í stafrófsröð sciName
+    // þá er notað sort en ekki filter, til að breyta röðuninni á array en ekki taka neitt út. 
+    // sort tekur inn 2 parametra til þess að geta forgangsraðað. 
 alphabet.addEventListener("click", (e) => {
     const nameOrder = myArray.sort((a, b) => {
+        // forgangsröðun, ef b er stærra en a, þá svissast þeir, en ef b er á undan þá helst röðunin. 
+        // annars 
         if (a.sciName < b.sciName) { return -1; }
         if (b.sciName > a.sciName) { return 1; }
         return 0;
     })
+    // hér er div-ið hreinsað svo það komi ekki nýjar niðurstöður undir fyrri niðurstöðum. 
+    // nameOrder er orðið array í röð sem við viljum hafa / ýttum á takkann fyrir, og svo skilum við 
+    //  cardinu inn í div-ið í HTML. 
     mainDiv.innerHTML = "";
     nameOrder.forEach(alphabet => {
         addCard(alphabet)
     }) 
     
 })
+// Hákon + Gummi 
